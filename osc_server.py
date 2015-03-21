78a71d730d01e4fb570e3c547a601f1d114c4799
Michael Kim <mmkim4@illinois.edu>
2015-03-21 15:53:17 -0500

    clean up + added python server code

--- osc_server.py
+++ osc_server.py
@@ -11,30 +11,43 @@ class MuseServer(ServerThread):
     @make_method('/muse/acc', 'fff')
     def acc_callback(self, path, args):
         acc_x, acc_y, acc_z = args
-        #print("%s %f %f %f" % (path, acc_x, acc_y, acc_z))
+        print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
+
+    #receive battery data
+    @make_method('/muse/batt', 'iiii')
+    def batt_callback(self, path, args):
+        state_charge, fuel_gauge, adc, temperature = args
+        print "%s %i %i %i %i" % (state_charge, fuel_gauge, adc, temperature)
 
     #receive EEG data
     @make_method('/muse/eeg', 'ffff')
     def eeg_callback(self, path, args):
         l_ear, l_forehead, r_forehead, r_ear = args
-        values = [l_ear, l_forehead, r_forehead, r_ear]
-        send(values,)
-        print("%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear))
+        print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+
+    #receive DRLREF data
+    #@make_method('/muse/drlref', 'ff')
+
+
+    #this block is devoted to picking up elements data [digital signal processing portions]
+    #receive Fourier Fast Trasform [for each channel]
+    #@make_method('/muse/element/raw_fft0', 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+
 
     #handle unexpected messages
     @make_method(None, None)
     def fallback(self, path, args, types, src):
-        print( "Unknown message \
-		\n\t Source: '%s' \
-		\n\t Address: '%s' \
-		\n\t Types: '%s ' \
-		\n\t Payload: '%s'" \
-		% (src.url, path, types, args))
+        print "Unknown message \
+        \n\t Source: '%s' \
+        \n\t Address: '%s' \
+        \n\t Types: '%s ' \
+        \n\t Payload: '%s'" \
+        % (src.url, path, types, args)
 
 try:
     server = MuseServer()
-except (ServerError, err):
-    print(str(err))
+except ServerError, err:
+    print str(err)
     sys.exit()
 
 server.start()
