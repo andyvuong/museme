a90213d4bfe22217441518f130cf2f411fe5c127
berwin7996 <berwin7996@gmail.com>
2015-03-22 11:57:43 -0500

    blahafsg

--- sound/s.ck
+++ sound/s.ck
@@ -5,13 +5,13 @@ SinOsc s1 => JCRev r1 => dac;
 SinOsc s2 => JCRev r2 => dac;
 SinOsc s3 => JCRev r3 => dac;
 SinOsc mel => JCRev melr => dac;
-.08 => s1.gain;
+.1 => s1.gain;
 .1 => r1.mix;
-.08 => s2.gain;
+.1 => s2.gain;
 .1 => r2.mix;
-.08 => s3.gain;
+.1 => s3.gain;
 .1 => r3.mix;
-.08 => mel.gain;
+.2 => mel.gain;
 .1 => melr.mix;
 
 // create our OSC receiver
@@ -22,7 +22,7 @@ OscRecv recv;
 recv.listen();
 
 // create an address in the receiver, store in new variable
-recv.event( "/foo/notes, i i i i i" ) @=> OscEvent @ oe;
+recv.event( "/out i i i i i" ) @=> OscEvent @ oe;
 
 // infinite event loop
 while( true )
@@ -41,12 +41,12 @@ while( true )
 
         // getFloat fetches the expected float (as indicated by "i f")
         oe.getInt() => i1 => Std.mtof => s1.freq;
-        oe.getInt() => i2 => Std.mtof => s2.freq;
+        oe.getInt() => i2; // => Std.mtof => s2.freq;
         oe.getInt() => i3 => Std.mtof => s3.freq;
-        oe.getInt() => i4 => Std.mtof => s4.freq;
+        oe.getInt() => i4; // => Std.mtof => s4.freq;
         oe.getInt() => i5 => Std.mtof => mel.freq;
 
         // print
-        <<< "got (via OSC):", i i i i i>>>;
+        <<< "got (via OSC):", i1, i2, i3, i4, i5 >>>;
     }
 }
--- sound/synth.py
+++ sound/synth.py
@@ -1,9 +1,10 @@
 from mingus.midi import fluidsynth
 import mingus.core.progressions as progressions
 import mingus.core.notes as notes
-from simpleOSC import initOSCClient, sendOSCMsg
-import time, math, random
+from simpleOSC import initOSCClient, sendOSCMsg, initOSCServer, startOSCServer, setOSCHandler
+import time, math, random, sys
 import threading
+
 
 try:
     from itertools import izip
@@ -11,26 +12,98 @@ except ImportError: # Python 3
     izip = zip
     xrange = range
 
+def process_mood(addr, tags, data, source):
+  print "received new osc msg from %s" % getUrlStr(source)
+  print "with addr : %s" % addr
+  print "typetags :%s" % tags
+  print "the actual data is : %s" % data
+  mood = data
+
 
 chords = [
-[],
-[],
-[],
-[],
-[],
-[]]
+['I', 'III', 'V', 'II'],
+['I', 'IV', 'V', 'ii'],
+['IV', 'viidim', 'iii', 'V'],
+['III', 'VII', 'VI', 'V'],
+['iidim', 'iv', 'v', 'III'],
+['IIdim', 'IVdim', 'IIIdim', 'VIdim']]
+
+tempo = [1, 0.5, 0.4, 0.3, 0.2, 0.2]
+
+#mood goes from 0-5, calm to angry
+#angry should cause faster and redder
+mood = 3
+
+
+
+
+eeg_waves = []
+a_waves = []
+b_waves = []
+
+
 
 ip = "localhost"
-port = 9002
+port1 = 9002
+port_receive = 9001
 base_a4=440
-initOSCClient(ip, port)
+
+
+port1 = sys.argv[1]
+
+initOSCClient(ip, int(port1))
+'''
+initOSCServer(ip, int(port_receive), 0)
+setOSCHandler('/mood', process_mood)
+startOSCServer()
+'''
 
 def mtof(midi):
 	return base_a4*2**((midi-9)/12.0)
 
-p = progressions.to_chords(['I', 'III', 'V', 'II'])
-print progressions.to_chords(p, "C")
-print(p)
+
+
+def process_eeg(addr, tags, data, source):
+
+  print "received new osc msg from %s" % getUrlStr(source)
+  print "with addr : %s" % addr
+  print "typetags :%s" % tags
+  print "the actual data is : %s" % data
+  avg = sum(data) / float(len(data))
+  eeg_waves.append(avg)
+  if(len(eeg_waves) > 100):
+    eeg_waves.pop(0)
+
+def process_alpha(addr, tags, data, source):
+
+  print "received new osc msg from %s" % getUrlStr(source)
+  print "with addr : %s" % addr
+  print "typetags :%s" % tags
+  print "the actual data is : %s" % data
+  avg = sum(data) / float(len(data))
+  a_waves.append(avg)
+  if(len(a_waves) > 100):
+    a_waves.pop(0)
+
+def process_beta(addr, tags, data, source):
+
+  print "received new osc msg from %s" % getUrlStr(source)
+  print "with addr : %s" % addr
+  print "typetags :%s" % tags
+  print "the actual data is : %s" % data
+  avg = sum(data) / float(len(data))
+  b_waves.append(avg)
+  if(len(b_waves) > 100):
+    b_waves.pop(0)
+
+def is_peak(freqs):
+  minf = min(freqs)
+  maxf = max(freqs)
+
+
+p = progressions.to_chords(chords[mood])
+print progressions.to_chords(p, "D")
+
 
 for k, chord in enumerate(p):
 	temp = []
@@ -47,40 +120,45 @@ chord_length = 4
 index = 0
 offset = 0
 speed = 1
+sleeptime = 0.5
+try:
+  while(1):
+    print i
+    note_play = random.randint(0, 10)
 
-while(1):
-  print i
-  note_play = random.randint(0, 10)
-
-  mood = 1
-  if mood > 3:
-      speed = 1.5
-
-  if note_play*speed >= 4:
-    offset = random.randint(-2,2)*11
-    index = random.randint(0, len(p[i])-1)
-    templist = p[curchord] + [mood] + [p[curchord][index]+offset]
-    sendOSCMsg("/out", [templist])
-    print 'played', templist
-    j += 1
-
-  
-  #every i intereations
-  elif i >= 4 and note_play != 0:
-    templist = p[curchord] + [mood] + [p[curchord][index]+offset]
-    sendOSCMsg("/out", [templist])
-    print 'played', templist
-
-  i += 1
-  
-  if (i >= len(p)):
-    curchord += 1
-    i = 0
-  if (j >= len(p)):
-    j = 0
-  if (curchord >= len(p)):
     
-    curchord = 0
+    if mood > 3:
+        sleeptime = tempo[mood]
+        speed = 3
 
-  time.sleep(0.75)
+    if note_play*speed >= 4:
 
+      offset = random.randint(-1,2)*11
+      index = random.randint(0, len(p[i])-1)
+      templist = p[curchord] + [mood] + [p[curchord][index]+offset]
+      sendOSCMsg("/out", [templist])
+      print 'played', templist
+      j += 1
+
+    
+    #every i intereations
+    elif i >= 4 and note_play != 0:
+      templist = p[curchord] + [mood] + [p[curchord][index]+offset]
+      sendOSCMsg("/out", [templist])
+      print 'played', templist
+
+    i += 1
+    
+    if (i >= len(p)):
+      curchord += 1
+      i = 0
+    if (j >= len(p)):
+      j = 0
+    if (curchord >= len(p)):
+      
+      curchord = 0
+
+    time.sleep(sleeptime)
+except (SystemExit, KeyboardInterrupt) as exc:
+  print "closing all OSC connections... and exit"
+  closeOSC()
