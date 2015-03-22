f806703cf51b67e64b5ecb19599835e43c42e2c2
Andy <andyv.42@gmail.com>
2015-03-22 09:11:39 -0500

    checked appjs and added parers

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ var val_beta = [];
 var val_delta = [];
 var val_theta = [];
 var val_gamma = [];
+var val_alpha_r = [];
+var val_beta_r = [];
+var val_delta_r = [];
+var val_theta_r = [];
+var val_gamma_r = [];
 var rang = [];
 
 // New OSC client that 
@@ -36,39 +41,66 @@ var oscServer = new osc.Server(port_osc, '0.0.0.0');
 function listen() {
 oscServer.on("message", function (msg, rinfo) {
       console.log("OSC Message:");
-      console.log(msg);
-      parsed = msg.split(" ");
-
+      console.log(typeof JSON.stringify(msg));
+      parsed = JSON.stringify(msg).split(" ");
+      console.log(parsed.length);
       path =  parsed[0];
       // PARSE CODE -> Push the average of the respective wave//
       if(path.localeCompare("/muse/elements/delta_absolute")) {
-          for(var i = 1; i < len(parsed); i++) {
+          for(var i = 1; i < parsed.length; i++) {
               val_delta.push(parsed[i]);
           }
       }
       else if (path.localeCompare("/muse/elements/theta_absolute")) {
-          for(var i = 1; i < len(parsed); i++) {
+          for(var i = 1; i < parsed.length; i++) {
               val_theta.push(parsed[i]);
           }
       }
       else if (path.localeCompare("/muse/elements/alpha_absolute")) {
-          for(var i = 1; i < len(parsed); i++) {
+          for(var i = 1; i < parsed.length; i++) {
               val_alpha.push(parsed[i]);
           }
        }
       else if (path.localeCompare("/muse/elements/beta_absolute")) {
-          for(var i = 1; i < len(parsed); i++) {
+          for(var i = 1; i < parsed.length; i++) {
               val_beta.push(parsed[i]);
           }
        }
       else if (path.localeCompare("/muse/elements/gamma_absolute")) {
-          for(var i = 1; i < len(parsed); i++) {
+          for(var i = 1; i < parsed.length; i++) {
               val_gamma.push(parsed[i]);
+          }
+       } /////
+      else if (path.localeCompare("/muse/elements/delta_resolute")) {
+          for(var i = 1; i < parsed.length; i++) {
+              val_delta_r.push(parsed[i]);
+          }
+       }
+      else if (path.localeCompare("/muse/elements/theta_resolute")) {
+          for(var i = 1; i < parsed.length; i++) {
+              val_theta_r.push(parsed[i]);
+          }
+       }
+      else if (path.localeCompare("/muse/elements/alpha_resolute")) {
+          for(var i = 1; i < parsed.length; i++) {
+              val_alpha_r.push(parsed[i]);
+          }
+       }
+      else if (path.localeCompare("/muse/elements/beta_resolute")) {
+          for(var i = 1; i < parsed.length; i++) {
+              val_beta_r.push(parsed[i]);
+          }
+       }
+      else if (path.localeCompare("/muse/elements/gamma_resolute")) {
+          for(var i = 1; i < parsed.length; i++) {
+              val_gamma_r.push(parsed[i]);
           }
        }
        else {
               console.log("Something weird happened!");
        }
+       console.log(msg);
+       console.log(parsed);
       //  Passes each of the 4 channels for each wave
       process(val_delta, val_theta, val_alpha, val_beta, val_gamma); 
       //process1(val); // pass values to chuck
