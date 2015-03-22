2e80f4c66cedef9146dad17532a0b5ffa07a576a
Andy <andyv.42@gmail.com>
2015-03-22 04:33:36 -0500

    new changes and components

--- app.js
+++ app.js
@@ -1,23 +1,77 @@
-var osc = require("osc"),
+var osc = require("node-osc"),
     http = require("http"),
     WebSocket = require("ws");
+var ajax = require('ajax');
+var express = require('express');
+var fs = require('fs');
+//var processing = require('processing-js');
 
-// Create an osc.js UDP Port listening on port 5001 . 
-var udpPort = new osc.UDPPort({
-    localAddress: "0.0.0.0",
-    localPort: 5001
-});
+// App begins
+var app = express();
+var routes  = require('./routes/index.js');
+app.use(express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + '/styles'));
  
-// Listen for incoming OSC bundles. 
-udpPort.on("bundle", function (oscBundle) {
-    console.log("An OSC bundle just arrived!", oscBundle);
-});
- 
-// Open the socket. 
-udpPort.open();
- 
-// Send an OSC message to, say, SuperCollider 
-udpPort.send({
-    address: "/s_new",
-    args: ["default", 100]
-}, "127.0.0.1", 5002);
+// Wolfram  base
+var base = 'https://www.wolframcloud.com/objects/37e0b60d-f828-4613-95e0-12a2c5aa1314';
+
+// Globals
+var port_running = 3000;
+var port_osc = 2000;
+var val = [];
+var rang = [];
+
+// New OSC client that 
+var client = new osc.Client('127.0.0.1', port_osc);
+client.send('/oscAddress', 200);
+
+// Osc client 
+var oscServer = new osc.Server(port_osc, '0.0.0.0');
+
+/**
+* Listens for incoming OSC messages.
+*/
+function listen() {
+oscServer.on("message", function (msg, rinfo) {
+      console.log("OSC Message:");
+      console.log(msg);
+      val.push(msg[1]);
+
+
+      process(val); // pass values to graph
+      //process1(val); // pass values to chuck
+      //process2(); // pass values to music
+});}
+
+/**
+* Processes the incoming values for the brain wave graph
+*/
+function process(item) {
+    if(item.length > 10) {
+        // do something with the array values
+
+        val = [];
+    }
+}
+
+/**
+* Processes the incoming values for emotions and subsequently music, visualization, and color changing
+*/
+function process(item) {
+    if(item.length > 10) {
+        // do something with the array values
+
+        val = [];
+    }
+}
+
+// Communicate between server and front-end
+
+
+
+// Start server
+listen();
+// "Front-facing" server
+console.log("Express server running on " + port_running);
+console.log("OSC server listening on " + port_osc);
+app.listen( port_running);
